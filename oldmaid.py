452aebfd89cbba489f60054509230f8cd881502f
ce691 <email@email.com>
2018-09-03 18:46:57 -0500

    old maid progress

--- oldmaid.py
+++ oldmaid.py
@@ -32,45 +32,50 @@ def gamePlay():
     print()
 
     ### Now we play...
+    # while there are more cards left to pick other than the old maid...
+
+    # TODO
+    # the while loop should only do one turn per loop
+    # must keep track of whose turn it is
 
     while playerHandClassVersion.getPlayerHandSize() + computerHandClassVersion.getComputerHandSize() > 1:
-    ### Pick a card from the computer's hand
-    size = computerHandClassVersion.getComputerHandSize()
-    print("Pick a card from the computer, enter a card number between 1 and", size)
-    index = input(" ")
-    while not (1 <= int(index) <= size):
-        index = input("Invalid number, try again.")
+        ### Pick a card from the computer's hand
+        size = computerHandClassVersion.getComputerHandSize()
+        print("Pick a card from the computer, enter a card number between 1 and", size)
+        index = input(" ")
+        while not (1 <= int(index) <= size):
+            index = input("Invalid number, try again.")
 
-    ### Check for / discard any new matches
-    tempCard = computerHandClassVersion.removeCard(int(index) - 1)
-    print("You picked this card: '", tempCard, "' and we will now check for & remove any matches... (PRESS ENTER)")
-    #playerHand.append(tempCard)
-    #playerHand = removeMatches(playerHand)
-    playerHandClassVersion.addCardRemoveMatches(tempCard)
-    print()
+        ### Check for / discard any new matches
+        tempCard = computerHandClassVersion.removeCard(int(index) - 1)
+        print("You picked this card: '", tempCard, "' and we will now check for & remove any matches... (PRESS ENTER)")
+        #playerHand.append(tempCard)
+        #playerHand = removeMatches(playerHand)
+        playerHandClassVersion.addCardRemoveMatches(tempCard)
+        print()
 
-    print("Here is your updated hand: ")
-    playerHandClassVersion.displayHand()
-    print()
+        print("Here is your updated hand: ")
+        playerHandClassVersion.displayHand()
+        print()
 
-    input("Press Enter to allow computer to take its turn...(PRESS ENTER)")
+        input("Press Enter to allow computer to take its turn...(PRESS ENTER)")
 
-    ### Computer picks a card from player hand
-    pHandSize = playerHandClassVersion.getPlayerHandSize()
-    randomIndex = random.randint(0, pHandSize-1)
-    tempCardForComputer = playerHandClassVersion.removeCard(randomIndex)
+        ### Computer picks a card from player hand
+        pHandSize = playerHandClassVersion.getPlayerHandSize()
+        randomIndex = random.randint(0, pHandSize-1)
+        tempCardForComputer = playerHandClassVersion.removeCard(randomIndex)
 
-    print("The computer picked '", tempCardForComputer, "'")
-    print()
+        print("The computer picked '", tempCardForComputer, "'")
+        print()
 
-    print("Here is your updated hand: ")
-    playerHandClassVersion.displayHand()
+        print("Here is your updated hand: ")
+        playerHandClassVersion.displayHand()
 
-    ### Check for / discard any new matches
-    computerHandClassVersion.addCardRemoveMatches(tempCardForComputer)
+        ### Check for / discard any new matches
+        computerHandClassVersion.addCardRemoveMatches(tempCardForComputer)
 
 
-    ### Play until 1 card left, declare winner.
+        ### Play until 1 card left, declare winner.
 
 
 def test():
