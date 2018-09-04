06e54fe20d8235c56cce8f989f2c1632dabb482e
ce691 <email@email.com>
2018-09-04 00:30:28 -0500

    added some exception handling

--- oldmaid_deck.py
+++ oldmaid_deck.py
@@ -5,16 +5,24 @@ class OldMaidDeck():
     def __init__(self):
         # Add our made up cards to the deck
         self.deck = []
-        #self.deck = ['bunny', 'bunny', 'dog', 'dog', 'kitty', 'kitty', 'bear', 'bear', 'mouse', 'mouse', 'duck', 'duck',
-                     #'horse', 'horse', 'frog', 'frog', 'bird', 'bird', 'skunk', 'skunk', 'cow', 'cow', 'pig', 'pig', 'old maid']
 
-        fin = open('deck_items.txt', 'rt')
-        while True:
-            line = fin.readline()
-            if not line:
-                break
-            self.deck.append(line.strip())
-        fin.close()
+        # Read the "cards" from a file
+        # In an old maid deck, every card has a match except for the Old Maid card
+        try:
+            fin = open('deck_items.txt', 'rt')
+            while True:
+                line = fin.readline()
+                if not line:
+                    break
+                self.deck.append(line.strip())
+            fin.close()
+
+        except Exception as e:
+            print("Error with the file", e)
+            # Set deck manually since the deck file didn't work
+            self.deck = ['bunny', 'bunny', 'dog', 'dog', 'kitty', 'kitty', 'bear', 'bear', 'mouse', 'mouse', 'duck', 'duck',
+                'horse', 'horse', 'frog', 'frog', 'bird', 'bird', 'skunk', 'skunk', 'cow', 'cow', 'pig', 'pig', 'old maid']
+            return
 
         # shuffle the deck
         random.shuffle(self.deck)
